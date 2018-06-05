7a27e5edb3d850ba63caac9da548dc4753a59343
William Ku <kucheech@gmail.com>
2018-06-05 20:44:19 +0800

    Initial commit

---
+++ app/src/main/java/sg/com/kaplan/soundbites/MainActivity.java
@@ -0,0 +1,25 @@
+package sg.com.kaplan.soundbites;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.hello);
+                mp.start();
+            }
+        });
+    }
+}
